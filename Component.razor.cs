29e5692afa74209af02265a26babce2a8ca59d4e
Mister Magoo <mister.magoo+githubsql@gmail.com>
2022-11-25 17:42:16 +0000

    Base commit

---
+++ Component.razor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using $rootnamespace$;
+
+namespace $rootnamespace$
+{
+    public partial class $safeitemname$
+    {
+    }
+}
